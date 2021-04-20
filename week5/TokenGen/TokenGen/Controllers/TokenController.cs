a40e3e3bc72e9daee81ba1384c5ccb9f738b85ba
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2021-04-20 15:37:07 -0500

    Token Generation

---
+++ week5/TokenGen/TokenGen/Controllers/TokenController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TokenGen.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        [HttpGet]
+        public dynamic Get()
+        {
+            return new
+            {
+                Guid = Guid.NewGuid().ToString(),
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = Environment.MachineName
+            };
+        }
+    }
+}
